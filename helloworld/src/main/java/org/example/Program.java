92fc013c47c473965cc279b8ad6b9d738c546a02
nhat01479 <nhat01479@gmail.com>
2023-04-07 10:29:24 +0700

    Java overview

---
+++ helloworld/src/main/java/org/example/Program.java
@@ -0,0 +1,53 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Program {
+
+  public static Scanner scanner = new Scanner(System.in);
+
+  public static void main(String[] args) {
+/*
+    System.out.println("Helloooooooooooo");
+
+
+    System.out.println("Nhập vào 1 chuỗi");
+    String b = scanner.nextLine();
+    System.out.println(b);
+
+
+    System.out.println("Nhập vào 1 số");
+    int a = scanner.nextInt();
+    System.out.println(a);
+
+    int a = 6;
+    int b = a++ + ++a*2;
+    System.out.println(a); //a = 8;
+    System.out.println(b); //b = 22;
+
+    int radius = 10;
+    final  double PI = 3.14;
+    double area = radius * radius * PI;
+    System.out.println(area);
+
+    int a = 130;
+    float b = 0.0f;
+    double c = 0.0;
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+
+
+    int a;
+    do {
+      System.out.println("Nhap vao mot so lon hon 0");
+      a = scanner.nextInt();
+    }
+    while (a < 0);
+*/
+
+
+
+  }
+}
+
