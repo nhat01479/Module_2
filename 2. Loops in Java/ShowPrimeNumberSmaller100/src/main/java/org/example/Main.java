655fcca4e3613d291e85579cf63c4a31ba21a8d3
nhat01479 <nhat01479@gmail.com>
2023-04-09 14:34:41 +0700

    Hiển thị các số nguyên tố nhỏ hơn 100

---
+++ 2. Loops in Java/ShowPrimeNumberSmaller100/src/main/java/org/example/Main.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Hiển thị các số nguyên tố nhỏ hơn: ");
+    int num = scanner.nextInt();
+    boolean check;
+
+    for (int i = 2; i < 1000; i++) {
+      check = true;
+      if (i < num) {
+        for (int j = 2; j <= Math.sqrt(i); j++) {
+          if (i % j == 0) {
+            check = false;
+            break;
+          }
+        }
+        if (check) {
+          System.out.println(i);
+        }
+      }
+    }
+  }
+}
