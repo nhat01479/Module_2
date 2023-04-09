9d47d2898b4fb170b4664aba8f520c48bc3b44a9
nhat01479 <nhat01479@gmail.com>
2023-04-09 08:35:18 +0700

    tinh lai suat

---
+++ 2. Loops in Java/Interest_Calculator/src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double money;
+        int month;
+        double interestRate;
+        System.out.println("Nhap so tien ban dau");
+        money = scanner.nextDouble();
+        System.out.println("Nhap so thang can gui");
+        month = scanner.nextInt();
+        System.out.println("Nhap lai suat");
+        interestRate = scanner.nextDouble();
+        double totalInterest = 0;
+        for (int i = 0; i < month; i++){
+            totalInterest += money * (interestRate/12) * month;
+        }
+        System.out.printf("Tien lai sau %d thang gui voi lai suat %.3f la %.3f", month, interestRate, totalInterest);
+    }
+}
