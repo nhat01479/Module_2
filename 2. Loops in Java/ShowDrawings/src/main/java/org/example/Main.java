e301e5504b34e54e241a3b4ff697818a2cc13cfd
nhat01479 <nhat01479@gmail.com>
2023-04-09 20:04:28 +0700

    [Bài tập] Hiển thị các loại hình

--- 2. Loops in Java/ShowDrawings/src/main/java/org/example/Main.java
+++ 2. Loops in Java/ShowDrawings/src/main/java/org/example/Main.java
@@ -7,7 +7,6 @@ public class Main {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     int choice = -1;
-    String str = "";
     while (choice != 4) {
       System.out.println("Menu");
       System.out.println("1. Print the rectangle");
@@ -16,6 +15,7 @@ public class Main {
       System.out.println("4. Exit");
       System.out.println("Enter your choice");
       choice = scanner.nextInt();
+      String str = "";
       switch (choice) {
         case 1:
           for (int i = 0; i < 4; i++) {
@@ -26,72 +26,74 @@ public class Main {
           }
           System.out.println(str);
           break;
-//        case 2:
-//          System.out.println("Choose direction of triangle");
-//          System.out.println("1. Top-left");
-//          System.out.println("2. Top-right");
-//          System.out.println("3. Bottom-left");
-//          System.out.println("4. Bottom-right");
-//          System.out.println("0. Exit");
-//          int direction = scanner.nextInt();
-//          while (direction != 0) {
-//            switch (direction) {
-//              case 1:
-//                for (int i = 0; i < 5; i++) {
-//                  for (int j = 0; j <= 5 - 1 - i; j++) {
-//                    str += "* ";
-//                  }
-//                  str += "\n";
-//                }
-//                System.out.println(str);
-//                break;
-//              case 2:
-//                for (int i = 0; i < 5; i++) {
-//                  for (int j = 0; j < 5; j++) {
-//                    if (j >= i) {
-//                      str += "* ";
-//                    } else {
-//                      str += "  ";
-//                    }
-//                  }
-//                  str += "\n";
-//                }
-//                System.out.println(str);
-//                break;
-//              case 3:
-//                for (int i = 0; i < 5; i++) {
-//                  for (int j = 0; j < 5; j++) {
-//                    if (j <= i) {
-//                      str += "* ";
-//                    } else {
-//                      str += "  ";
-//                    }
-//                  }
-//                  str += "\n";
-//                }
-//                System.out.println(str);
-//                break;
-//              case 4:
-//                for (int i = 0; i < 5; i++) {
-//                  for (int j = 0; j < 5; j++) {
-//                    if (j >= 5 - 1 - i) {
-//                      str += "* ";
-//                    } else {
-//                      str += "  ";
-//                    }
-//                  }
-//                  str += "\n";
-//                }
-//                System.out.println(str);
-//                break;
-//              case 0:
-//                System.out.println("Exit");
-//                break;
-//              default:
-//                System.out.println("No choice");
-//            }
-//            break;
-//          }
+        case 2:
+          int direction = -1;
+          while (direction != 5) {
+            System.out.println("Choose direction of triangle");
+            System.out.println("1. Top-left");
+            System.out.println("2. Top-right");
+            System.out.println("3. Bottom-left");
+            System.out.println("4. Bottom-right");
+            System.out.println("5. Exit");
+            direction = scanner.nextInt();
+            str = "";
+            switch (direction) {
+              case 1:
+                for (int i = 0; i < 5; i++) {
+                  for (int j = 0; j <= 5 - 1 - i; j++) {
+                    str += "* ";
+                  }
+                  str += "\n";
+                }
+                System.out.println(str);
+                break;
+              case 2:
+                for (int i = 0; i < 5; i++) {
+                  for (int j = 0; j < 5; j++) {
+                    if (j >= i) {
+                      str += "* ";
+                    } else {
+                      str += "  ";
+                    }
+                  }
+                  str += "\n";
+                }
+                System.out.println(str);
+                break;
+              case 3:
+                for (int i = 0; i < 5; i++) {
+                  for (int j = 0; j < 5; j++) {
+                    if (j <= i) {
+                      str += "* ";
+                    } else {
+                      str += "  ";
+                    }
+                  }
+                  str += "\n";
+                }
+                System.out.println(str);
+                break;
+              case 4:
+                for (int i = 0; i < 5; i++) {
+                  for (int j = 0; j < 5; j++) {
+                    if (j >= 5 - 1 - i) {
+                      str += "* ";
+                    } else {
+                      str += "  ";
+                    }
+                  }
+                  str += "\n";
+                }
+                System.out.println(str);
+                break;
+              case 0:
+                System.out.println("Exit");
+                break;
+              default:
+                System.out.println("No choice");
+            }
+          }
+          break;
         case 3:
           for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 12; j++) {
