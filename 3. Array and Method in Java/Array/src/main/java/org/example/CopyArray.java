8561e3a61f78ccf2f06e120cc948c672dac3fdb4
nhat01479 <nhat01479@gmail.com>
2023-04-10 11:07:59 +0700

    sao chép mảng

---
+++ 3. Array and Method in Java/Array/src/main/java/org/example/CopyArray.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class CopyArray {
+    public static void main(String[] args) {
+/*  Sao chép mảng
+        int[] a = {1,2,3,5,4};
+        int[] b = new int[a.length];
+        for (int i = 0; i < a.length; i++){
+            b[i] = a[i];
+            System.out.printf("b[%d] - %d \n", i, b[i]);
+        }
+*/
+
+        int[] a = {1,2,3,5,4};
+        int[] b = new int[a.length];
+        System.arraycopy(a, 0, b, 0, a.length);
+        for (int i = 0; i < b.length; i++){
+            System.out.printf("b[%d] - %d \n", i, b[i]);
+        }
+    }
+}
