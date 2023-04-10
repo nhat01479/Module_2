dd307e68502e8ceba7a0f7285dd1ac8b346d4c16
nhat01479 <nhat01479@gmail.com>
2023-04-10 16:39:08 +0700

    kiểm tra số nguyên tố

--- 2. Loops in Java/PrimeNumber/src/main/java/org/example/Main.java
+++ 2. Loops in Java/PrimeNumber/src/main/java/org/example/Main.java
@@ -4,23 +4,26 @@ import java.util.Scanner;
 
 public class Main {
 
-  public static Scanner scanner = new Scanner(System.in);
+    public static Scanner scanner = new Scanner(System.in);
 
-  public static void main(String[] args) {
-    System.out.println("Nhap so can check");
-    int num = scanner.nextInt();
-    int count = 0;
-    if (num < 2)
-      System.out.println(num + " khong phai so nguyen to");
-    for (int i = 2; i <= Math.sqrt(num); i++) {
-      if (num % i == 0) {
-        count++;
-      }
+    public static void main(String[] args) {
+        System.out.println("Nhap so can check");
+        int num = scanner.nextInt();
+        boolean check = true;
+        if (num < 2)
+            check = false;
+        else {
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    check = false;
+                    break;
+                }
+            }
+            if (check) {
+                System.out.println(num + " la so nguyen to");
+            } else {
+                System.out.println(num + " khong phai so nguyen to");
+            }
+        }
     }
-    if (count == 0) {
-      System.out.println(num + " la so nguyen to");
-    } else {
-      System.out.println(num + " khong phai so nguyen to");
-    }
-  }
 }
