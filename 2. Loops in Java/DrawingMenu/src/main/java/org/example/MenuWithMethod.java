afd3f5c833332f772eaebef43b2a7f3f36f376b4
nhat01479 <nhat01479@gmail.com>
2023-04-11 08:15:23 +0700

    chống trôi

--- 2. Loops in Java/DrawingMenu/src/main/java/org/example/MenuWithMethod.java
+++ 2. Loops in Java/DrawingMenu/src/main/java/org/example/MenuWithMethod.java
@@ -16,7 +16,7 @@ launcher();
             System.out.println("1. Draw the triangle");
             System.out.println("2. Draw the square");
             System.out.println("3. Draw the rectangle");
-            int choice = Integer.parseInt(scanner.nextLine());
+            int choice = scanner.nextInt();
             switch (choice) {
                 case 1:
                     exercisePrintTriangle();
