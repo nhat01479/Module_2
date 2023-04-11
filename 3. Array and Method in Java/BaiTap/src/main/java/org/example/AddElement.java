bcd204d8d69bb945d0127d130fd9f2b22e3bb15b
nhat01479 <nhat01479@gmail.com>
2023-04-11 17:28:48 +0700

    [Bài tập] Chèn phần tử vào mảng

--- 3. Array and Method in Java/BaiTap/src/main/java/org/example/AddElement.java
+++ 3. Array and Method in Java/BaiTap/src/main/java/org/example/AddElement.java
@@ -1,4 +1,47 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class AddElement {
+    public static void main(String[] args) {
+        int[] myList = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+//        myList = addElement(7, myList);
+//        System.out.println(Arrays.toString(myList));
+        System.out.println(Arrays.toString(myList));
+
+        int value = 10;
+        int indexofvalue = 9;
+
+        if (indexofvalue < 0 || indexofvalue > myList.length - 1) {
+            System.out.println("Khong them duoc");
+        } else {
+            myList = moveElementToIndex(indexofvalue, value, myList);
+        }
+
+        System.out.println(Arrays.toString(myList));
+    }
+
+    //Add element vào cuối mảng
+    public static int[] addElement(int value, int[] numbers) {
+        int[] newList = new int[numbers.length + 1];
+        for (int i = 0; i < numbers.length; i++) {
+            newList[i] = numbers[i];
+        }
+        newList[newList.length - 1] = value;
+        return newList;
+    }
+
+    public static int[] moveElementToIndex(int index, int value, int[] numbers) {
+
+        int[] newList = new int[numbers.length + 1];
+        for (int i = 0; i < index; i++) {
+            newList[i] = numbers[i];
+        }
+        newList[index] = value;
+        for (int i = index; i < newList.length - 1; i++) {
+            newList[i + 1] = numbers[i];
+        }
+        return newList;
+
+    }
 }
