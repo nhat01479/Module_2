836de8498f34e9aca091b0caf606d22b3382b678
nhat01479 <nhat01479@gmail.com>
2023-04-12 20:31:33 +0700

    Demo Class và Object

--- 4. OPP/ClassInJava/src/main/java/org/example/Main.java
+++ 4. OPP/ClassInJava/src/main/java/org/example/Main.java
@@ -7,9 +7,10 @@ public class Main {
     public static void main(String[] args) {
 
         /* Class và Object
-        -	Là đơn vị thực thi cơ bản trong Java
--	Quy định thuộc tính và hành vi của các đối tượng
--	Khai báo lớp ==> sẽ khai báo 1 KIỂU DỮ LIỆU MỚI ==> để có thể khởi tạo các đối tượng thuộc kiểu dữ liệu đó
+        Class
+ -	Là đơn vị thực thi cơ bản trong Java
+ -	Quy định thuộc tính và hành vi của các đối tượng
+ -	Khai báo lớp ==> sẽ khai báo 1 KIỂU DỮ LIỆU MỚI ==> để có thể khởi tạo các đối tượng thuộc kiểu dữ liệu đó
 
 -	Cú pháp:
 			class class_name {
@@ -17,15 +18,16 @@ public class Main {
 			}
 
 		+ class body: nơi khai báo các thành phần:
-		    .	field (trường), phương thức (method)
+		    .	data field (trường dữ liệu)
+		    .   phương thức (method)
 		    .	contructor: phương thức khởi tạo ==> để khởi tạo các đối tượng của lớp
-					có TÊN TRÙNG VỚI TÊN LỚP
-					1 lớp có thể có nhiều contructor
-					nếu không khai báo thì mặc định lớp có 1 contructor không có tham số
+					        có TÊN TRÙNG VỚI TÊN LỚP
+				        	1 lớp có thể có nhiều contructor
+				        	nếu không khai báo thì mặc định lớp có 1 contructor không có tham số
 
 -	Quy tắc đặt tên: 	quy tắc lạc đà
-				không trùng các từ khoá
-				bắt đầu bằng ($, _, hoặc chữ cái)
+				        không trùng các từ khoá
+				        bắt đầu bằng ($, _, hoặc chữ cái)
 
 -	Khởi tạo đối tượng (object): sau khi khai báo lớp
 
@@ -37,16 +39,23 @@ public class Main {
 
 -	Ví dụ:	Person personObj = new Person;
 		hoặc
-			Person personObj;
-			personObj = new Person();
+			Person personObj;           //khai báo biến: biến personObj được khai báo và không trỏ đến đối tượng nào, có giá trị null
+			personObj = new Person();   //khởi tạo đối tượng new Person() và lưu vào vùng nhớ, địa chỉ vùng nhớ được gán cho biến personObj
 
--	Truy xuất các thành phần của đối tượng:
+-	Truy xuất các thành phần của đối tượng: thông qua biến trỏ đến đối tượng
 
-			personObj.property
+			personObj.property;
 
 -	Gọi phương thức của đối tượng:
 
 			personObj.method();
-         */
+
+-   Khai báo getter/ setter
+        + getter:       public returnType getPropertyName() {}    //return this.property = property;
+        + setter:       public returnType setPropertyName()	{}    //this.property = property;
+        + boolean:      public boolean isPropertyName()	{}
+-   this: đại diện cho đối tượng hiện tại
+
+ */
     }
 }
