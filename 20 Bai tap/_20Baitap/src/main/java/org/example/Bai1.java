6441a4b447b7f6c1dbef1449762bc05f539ce545
nhat01479 <nhat01479@gmail.com>
2023-04-13 16:53:02 +0700

    Bài 1: Hãy liệt kê các giá trị có toàn chữ số lẻ trong mảng 1 chiều các số nguyên

--- 20 Bai tap/_20Baitap/src/main/java/org/example/Bai1.java
+++ 20 Bai tap/_20Baitap/src/main/java/org/example/Bai1.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Bai1 {
     public static void main(String[] args) {
-        int[] numbers = {1, 2, 5, 10, 11, 13, 16, 120, 131, 157};
+        int[] numbers = {1, -2, -5, 10, 11, 13, 16, 120, -131, 157};
         System.out.println(Arrays.toString(numbers));
         /*
         for (int i = 0; i < numbers.length; i++) {
@@ -24,10 +24,26 @@ public class Bai1 {
                 System.out.println(numbers[i] + " ");
         }
          */
+        /*
+        for (int i = 0; i < numbers.length; i++) {
+            int num = Math.abs(numbers[i]);
+            boolean checkOdd = true;
+            while (num > 0) {
+                int digits = num % 10;
+                if (digits % 2 == 0){
+                    checkOdd = false;
+                    break;
+                }
+                num /= 10;
+            }
+            if (checkOdd)
+                System.out.print(numbers[i] + " ");
+        }
+        */
         for (int i = 0; i < numbers.length; i++) {
             int num = numbers[i];
             boolean checkOdd = true;
-            while (num > 0) {
+            while (num != 0) {
                 int digits = num % 10;
                 if (digits % 2 == 0){
                     checkOdd = false;
