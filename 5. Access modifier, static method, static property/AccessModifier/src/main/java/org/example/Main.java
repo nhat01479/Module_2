428237aef5b2b36c00feaeaf3ae3f8e326dd86bc
nhat01479 <nhat01479@gmail.com>
2023-04-14 10:57:29 +0700

    5. Access modifier, static method, static property

---
+++ 5. Access modifier, static method, static property/AccessModifier/src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static Scanner scanner = new Scanner(System.in);
+    /*
+ Access modifier là các từ khoá được sử dụng để quy định mức độ truy cập đến lớp và các thành phần của lớp
+    +   public: có thể truy cập từ bất cứ đâu;
+    +   private: các phương thức và thuộc tính chỉ được phép truy xuất trong cùng một lớp;
+    +   protected: các PT và thuộc tính được phép truy xuất trong cùng một lớp và ở các lớp con (kế thừa);
+    +   default: nếu không có access modifier thì mức default sẽ được áp dụng. Lớp và các thành phần của lớp được truy xuất ở những nơi trong cùng một package
+
+  Ví du:
+    https://prnt.sc/gABMT-k8neGG
+
+Nested class và anonymous class
+
+Nested class (lớp lồng nhau) là một lớp được khai báo bên trong lớp khác
+
+    * */
+    public static void main(String[] args)
+    {
+
+    }
+}
