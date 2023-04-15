8191ca2c01b46bc538099dfd7db549c75e85a144
nhat01479 <nhat01479@gmail.com>
2023-04-15 22:26:33 +0700

    [Bài tập] Access modifier

---
+++ 5. Access modifier, static method, static property/AccessModifier/src/main/java/org/example/Circle.java
@@ -0,0 +1,31 @@
+package org.example;
+//[Bài tập] Access modifier
+
+public class Circle {
+
+  public static void main(String[] args) {
+    Circle circle1 = new Circle();
+    System.out.println(circle1.getRadius());
+    System.out.println(circle1.getArea());
+  }
+
+  private double radius;
+  private String color;
+
+  public Circle() {
+    this.radius = 1;
+    this.color = "red";
+  }
+
+  public Circle(double radius) {
+    this.radius = radius;
+  }
+
+   protected double getRadius() {
+    return this.radius;
+  }
+
+  public double getArea() {
+    return radius * radius * Math.PI;
+  }
+}
