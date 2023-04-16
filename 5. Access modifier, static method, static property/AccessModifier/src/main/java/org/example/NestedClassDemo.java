77a40922970b1d3b3c7fa128d455e4242bd62577
nhat01479 <nhat01479@gmail.com>
2023-04-16 13:44:43 +0700

    Lớp lồng nhau

---
+++ 5. Access modifier, static method, static property/AccessModifier/src/main/java/org/example/NestedClassDemo.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import org.example.OuterClass.StaticNestedClass;
+
+public class NestedClassDemo {
+
+  public static void main(String[] args) {
+    // Để truy cập lớp bên trong, phải tạo 1 đối tượng của lớp bên ngoài
+    OuterClass outer = new OuterClass();
+
+    // Sau đó tạo 1 đối tượng của lớp bên trong
+    OuterClass.InnerClass inner = outer.new InnerClass();
+    inner.innerShow();
+
+    //static nested class: có thể truy cập lớp bên trong mà không cần tạo một đối tượng của lớp bên ngoài
+    OuterClass.StaticNestedClass nestedClass = new OuterClass.StaticNestedClass();
+    nestedClass.staticNestedShow();
+  }
+}
+/*********************************************************/
+class OuterClass { //Outer class
+//  public void outerShow(){
+//    InnerClass inner = new InnerClass();
+//    inner.innerShow();
+//  }
+
+ // static nested class
+  static class StaticNestedClass{
+      public void staticNestedShow(){
+        System.out.println("Hello, đây là static nested class");
+      }
+  }
+  // inner class (non-static)
+  class InnerClass{
+    public void innerShow(){
+      System.out.println("Hello, đây là inner class");
+    }
+  }
+}
