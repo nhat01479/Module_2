0ad4d56be99ecc4c0d17a281d1a04183f310b4d0
nhat01479 <nhat01479@gmail.com>
2023-04-17 16:39:20 +0700

    StopWatch

--- 4. OPP/ClassInJava/src/main/java/org/example/StopWatch.java
+++ 4. OPP/ClassInJava/src/main/java/org/example/StopWatch.java
@@ -15,11 +15,11 @@ public class StopWatch {
         StopWatch watch = new StopWatch();
         watch.start();
 
-        sortArray(a);
+        sort(a);
 
         watch.stop();
         System.out.println(Arrays.toString(a));
-        System.out.println("Thoi gian sap xep: "+ watch.getElapsedTime() + " milliseconds");
+        System.out.println("Thoi gian sap xep: " + watch.getElapsedTime() + " milliseconds");
     }
 
     private LocalTime startTime;
@@ -45,16 +45,17 @@ public class StopWatch {
         this.endTime = LocalTime.now();
     }
 
-    public long getElapsedTime(){
-        long times = (endTime.toNanoOfDay() - startTime.toNanoOfDay())/1000000;
+    public long getElapsedTime() {
+        long times = (endTime.toNanoOfDay() - startTime.toNanoOfDay()) / 1000000;
         return times;
     }
-//    public Duration getElapsedTime() {
+
+    //    public Duration getElapsedTime() {
 //        Duration time = Duration.between(this.startTime, this.endTime);
 //        return time;
 //    }
 // Di voi         System.out.println(watch.getElapsedTime().toMillis());
-    public static int[] sortArray(int[] a){
+    public static int[] sortArray(int[] a) {
         for (int i = 0; i < a.length - 1; i++) {
             for (int j = i + 1; j < a.length; j++) {
                 if (a[j] > a[i]) {
@@ -65,6 +66,42 @@ public class StopWatch {
             }
         }
         return a;
+    }
+
+    public static void sort(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return;
+        }
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    private static void quickSort(int[] arr, int left, int right) {
+        int i = left;
+        int j = right;
+        int pivot = arr[left + (right - left) / 2];
+
+        while (i <= j) {
+            while (arr[i] < pivot) {
+                i++;
+            }
+            while (arr[j] > pivot) {
+                j--;
+            }
+            if (i <= j) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+        if (left < j) {
+            quickSort(arr, left, j);
+        }
+        if (i < right) {
+            quickSort(arr, i, right);
+        }
     }
 }
 
