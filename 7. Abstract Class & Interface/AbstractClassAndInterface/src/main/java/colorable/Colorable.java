0022cb58b6e854fd0bfe7589ebfb1e59bd6ca675
nhat01479 <nhat01479@gmail.com>
2023-04-19 23:49:43 +0700

    [Bài tập] triển khai interface 'Colorable' cho các lớp hình học

---
+++ 7. Abstract Class & Interface/AbstractClassAndInterface/src/main/java/colorable/Colorable.java
@@ -0,0 +1,6 @@
+package colorable;
+
+public interface Colorable {
+  void howToColor();
+
+}
---
+++ 7. Abstract Class & Interface/AbstractClassAndInterface/src/main/java/colorable/Test.java
@@ -0,0 +1,16 @@
+package colorable;
+
+public class Test {
+
+  public static void main(String[] args) {
+    Shape[] shapes = new Shape[3];
+    shapes[0] = new Circle(10);
+    shapes[1] = new Rectangle(5,10);
+    shapes[2] = new Square(20);
+    for (Shape shape: shapes){
+      if (shape instanceof Colorable){
+        ((Colorable) shape).howToColor();
+      }
+    }
+  }
+}
