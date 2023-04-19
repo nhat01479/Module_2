9ca1da82e145dfee511b144f901298a6f9738f8b
nhat01479 <nhat01479@gmail.com>
2023-04-19 09:08:50 +0700

    Demo Abstract class

---
+++ 7. Abstract Class & Interface/AbstractClassAndInterface/src/main/java/test_abstrack/Rectangle.java
@@ -0,0 +1,32 @@
+package test_abstrack;
+
+public class Rectangle extends Shape{
+    private double width, length;
+
+    public Rectangle(Coordinates coor, double width, double length) {
+        super(coor);
+        this.width = width;
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    @Override
+    public double getArea() {
+        return width * length;
+    }
+}
