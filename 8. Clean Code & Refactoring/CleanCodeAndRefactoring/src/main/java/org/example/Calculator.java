7cbf275e4e7836ee0b5005fcc2d3b1d531623168
nhat01479 <nhat01479@gmail.com>
2023-04-20 19:52:18 +0700

    [Thực hành] Refactoring - đổi tên biến và tách hằng

---
+++ 8. Clean Code & Refactoring/CleanCodeAndRefactoring/src/main/java/org/example/Calculator.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class Calculator {
+
+  public static final char ADDITION = '+';
+  public static final char SUBTRACTION = '-';
+  public static final char MULTIPLICATION = '*';
+  public static final char DIVISION = '/';
+  public static int calculate(int firstOperand, int secondOperand, char operator) {
+    int firstOperand1 = firstOperand;
+    switch (operator) {
+      case ADDITION:
+        return firstOperand1 + secondOperand;
+      case SUBTRACTION:
+        return firstOperand1 - secondOperand;
+      case MULTIPLICATION:
+        return firstOperand1 * secondOperand;
+      case DIVISION:
+        if (secondOperand != 0)
+          return firstOperand1 / secondOperand;
+        else
+          throw new RuntimeException("Can not divide by 0");
+      default:
+        throw new RuntimeException("Unsupported operation");
+    }
+  }
+}
