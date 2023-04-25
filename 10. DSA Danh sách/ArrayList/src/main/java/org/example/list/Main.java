d65a25624a1774b38faa9c5a826c609c6d57bf9e
nhat01479 <nhat01479@gmail.com>
2023-04-25 21:31:29 +0700

    demo cac thao tac co ban

--- 10. DSA Danh sách/ArrayList/src/main/java/org/example/list/Main.java
+++ 10. DSA Danh sách/ArrayList/src/main/java/org/example/list/Main.java
@@ -1,6 +1,9 @@
 package org.example.list;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -10,6 +13,7 @@ public class Main {
         listArray.add(1);
         listArray.add(2);
         listArray.add(3);
+        System.out.println("-------------------------------");
         LinkedList<Integer> list = new LinkedList<>();
         list.add(1);
         list.add(2);
@@ -17,10 +21,22 @@ public class Main {
         list.addFirst(1);
         list.addLast(5);
         list.add(1,6);
+        //add mang
+        list.addAll(Arrays.asList(10,11,12));
+        list.addAll(listArray);
+        //Sap xep
+        Collections.sort(list);
+        Collections.sort(list,Collections.reverseOrder());
+        //So lan xuat hien cua phan tu
+        System.out.println(Collections.frequency(list,3));
 
-//        list.removeLast();
-        System.out.println(list.remove(1));
-        //        System.out.println(list.removeFirst());
+
+        //Duyet va in ra cac phan tu
+//        list.forEach(x -> System.out.printf("%d\t\t", x));
+        for (Integer list1: list){
+            System.out.printf("%d\t\t", list1);
+        }
+        System.out.println();
         System.out.println(list);
     }
 }
