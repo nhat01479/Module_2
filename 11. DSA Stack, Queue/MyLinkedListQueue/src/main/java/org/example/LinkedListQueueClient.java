2d961e18a2330bfb04b905ebf48f6521a141063c
nhat01479 <nhat01479@gmail.com>
2023-04-27 16:38:30 +0700

    [Thực hành] Triển khai Queue sử dụng danh sách liên kết

--- 11. DSA Stack, Queue/MyLinkedListQueue/src/main/java/org/example/LinkedListQueueClient.java
+++ 11. DSA Stack, Queue/MyLinkedListQueue/src/main/java/org/example/LinkedListQueueClient.java
@@ -1,2 +1,17 @@
-package org.example;public class LinkedListQueueClient {
+package org.example;
+
+public class LinkedListQueueClient {
+    public static void main(String[] args) {
+        MyLinkedListQueue queue = new MyLinkedListQueue();
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.dequeue();
+        queue.dequeue();
+        queue.enqueue(30);
+        queue.enqueue(40);
+        System.out.println("Dequeued item is " +  queue.dequeue().key);
+
+    }
+
+
 }
--- 11. DSA Stack, Queue/MyLinkedListQueue/src/main/java/org/example/Main.java
+++ 11. DSA Stack, Queue/MyLinkedListQueue/src/main/java/org/example/Main.java
@@ -1,2 +1,17 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(4);
+        queue.add(2);
+        queue.add(5);
+        System.out.println(queue.add(5));
+        System.out.println(queue.remove());
+    }
 }
