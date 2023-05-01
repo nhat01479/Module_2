0ae7ccf6cfa2ba2bff9739184c14f01dcbaa57a1
nhat01479 <nhat01479@gmail.com>
2023-05-01 21:13:36 +0700

    [Bài tập] Tìm chuỗi tăng dần có độ dài lớn nhất

---
+++ 13. Search/LargestAscendingString/src/main/java/org/example/Main.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.util.LinkedList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "Abcabcd";
+
+        LinkedList<Character> max = new LinkedList<>();
+        for (int i = 0; i < str.length(); i++){
+            LinkedList<Character> list = new LinkedList<>();
+            list.add(str.charAt(i));
+            for (int j = i + 1; j < str.length(); j++){
+                if (str.charAt(j) > list.getLast())
+                    list.add(str.charAt(j));
+            }
+            if (list.size() > max.size()){
+                max.clear();
+                max.addAll(list);
+            }
+            list.clear();
+        }
+        for (Character character: max){
+            System.out.print(character);
+        }
+
+
+
+    }
+}
