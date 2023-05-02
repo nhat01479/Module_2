cf4cfc7a923752d8af0340d988a50cadbdf7e8c9
nhat01479 <nhat01479@gmail.com>
2023-05-02 18:06:54 +0700

    Quick Sort

---
+++ 14. Sort/QuickSort/src/main/java/org/example/QuickSort.java
@@ -0,0 +1,68 @@
+package org.example;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] arr = new int[100000];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int)(Math.random()*100);
+        }
+        int n = arr.length;
+
+        System.out.println("Mảng ban đầu:");
+        printArray(arr);
+
+        QuickSort ob = new QuickSort();
+        ob.sort(arr, 0, n - 1);
+
+        System.out.println("Mảng sau khi sắp xếp:");
+        printArray(arr);
+    }
+    // Hàm nhận phần tử cuối cùng làm chốt,
+    // đặt các phần tử nhỏ hơn chốt ở trước
+    // và lớn hơn ở sau nó
+    int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1); // index of smaller element
+        for (int j = low; j < high; j++) {
+
+            // Nếu phần tử hiện tại nhỏ hơn chốt
+            if (arr[j] < pivot) {
+                i++;
+
+                // swap arr[i] và arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        // swap arr[i+1] và arr[high] (hoặc pivot)
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    // arr[] --> Mảng cần được sắp xếp
+    // low --> chỉ mục bắt đầu
+    // high --> chỉ mục kết thúc
+    void sort(int[] arr, int low, int high) {
+        if (low < high) {
+
+            // pi là chỉ mục của chốt, arr[pi] vị trí của chốt
+            int pi = partition(arr, low, high);
+
+            // Sắp xếp đệ quy các phần tử
+            // trướcphân vùng và sau phân vùng
+            sort(arr, low, pi - 1);
+            sort(arr, pi + 1, high);
+        }
+    }
+
+
+    static void printArray(int[] arr) {
+        for (int j : arr) System.out.print(j + " ");
+        System.out.println();
+    }
+}
